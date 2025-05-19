5cf6fca70843f780cd274603f4b52cf0cbe6d436
FernandoLF <FernandoLopez29@gmail.com>
2025-05-19 15:30:01 -0600

    Service Provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,6 +1,4 @@
 <?php
-/AuthServiceProvider.php
-<?php
 
 namespace App\Providers;
 
@@ -20,6 +18,30 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [
+        //
+    ];
+
+    /**
+     * Register any events for your application.
      */
     public function boot(): void
     {
